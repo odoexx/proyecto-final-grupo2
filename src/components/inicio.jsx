246f62e2d1ad6eced9bf7038efa467a87ebf97bb
Gryuker <eskeleto@1958.outlook.es>
2022-11-13 19:45:22 -0300

    Agustin: Ajustes en Inicio

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -10,17 +10,72 @@ function Inicio() {
           Fundamentos de Programacion Web.
           </h3>
         <p id="inicio-texto1">
-          Bienvenidos al juego Arkanoid realizado por el Grupo 2.
+          Bienvenidos al Proyecto Final realizado por el Grupo 2.
         </p>
       </section>
       <section className="contenedor">
+      <h3 id="inicio-titulo">
+          Ahorcadito
+          </h3>
         <Button
           className="boton-inicio"
           variant="primary"
           size="lg"
           border="ligth"
           onClick={()=>sonidoClic.play()}
-          href={"./juego"}
+          href={"./ahorcadito"}
+        />
+      </section>
+      <section className="contenedor">
+      <h3 id="inicio-titulo">
+          Arkanoid
+          </h3>
+        <Button
+          className="boton-inicio2"
+          variant="primary"
+          size="lg"
+          border="ligth"
+          onClick={()=>sonidoClic.play()}
+          href={"./arkanoid"}
+        />
+      </section>
+      <section className="contenedor">
+      <h3 id="inicio-titulo">
+          Geometry Dash
+          </h3>
+        <Button
+          className="boton-inicio3"
+          variant="primary"
+          size="lg"
+          border="ligth"
+          onClick={()=>sonidoClic.play()}
+          href={"./geometrydash"}
+        />
+      </section>
+      <section className="contenedor">
+      <h3 id="inicio-titulo">
+          MemoTest
+          </h3>
+        <Button
+          className="boton-inicio4"
+          variant="primary"
+          size="lg"
+          border="ligth"
+          onClick={()=>sonidoClic.play()}
+          href={"./memotest"}
+        />
+      </section>
+      <section className="contenedor">
+      <h3 id="inicio-titulo">
+          Piedra, Papel o Tijeras
+          </h3>
+        <Button
+          className="boton-inicio5"
+          variant="primary"
+          size="lg"
+          border="ligth"
+          onClick={()=>sonidoClic.play()}
+          href={"./PPT"}
         />
       </section>
     </>
